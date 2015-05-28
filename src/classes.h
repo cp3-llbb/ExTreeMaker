5d588781c064e01eb6b087d026983c72b9b827f1
Olivier <olivier.bondu@gmail.com>
2015-05-28 17:29:59 +0200

    Deleting c++ code from the area

--- src/classes.h
+++
@@ -1,5 +0,0 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-
-//Add includes for your classes here
-#include "cp3-llbb/ExTreeMaker/interface/btagPerfFWLiteInterface.h"
-#include "cp3-llbb/ExTreeMaker/interface/BTagWeight.h"
