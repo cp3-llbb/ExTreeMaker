ac64e7c4d6dac51463fde5fe6e7892730cf53cfc
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-06-05 13:55:33 +0200

    small optimization

--- python/Core/ProductManager.py
+++ python/Core/ProductManager.py
@@ -3,12 +3,13 @@ __author__ = 'sbrochet'
 class ProductManager:
 
     class ProductWrapper:
+
         def __init__(self, prefix, instance):
             self._prefix = prefix
             self._instance = instance
 
         def __getattr__(self, item):
-            if not hasattr(self._instance, item):
+            if not item.startswith(self._prefix):
                 item = self._prefix + item
 
             return self._instance.__getattr__(item)
