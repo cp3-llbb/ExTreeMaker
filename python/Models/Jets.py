d2c9731df66e4c407f0d211e030f16548ac97444
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-06-07 15:01:52 +0200

    Store more information about jets

--- python/Models/Jets.py
+++ python/Models/Jets.py
@@ -5,4 +5,12 @@ from Candidates import Candidates
 import ROOT
 
 class Jets(Candidates):
+    area = ROOT.vector('float')
+    partonFlavor = ROOT.vector('int8_t')
+    hadronFlavor = ROOT.vector('int8_t')
+    jecFactor = ROOT.vector('float')
+
     pu_jet_id = ROOT.vector('float')
+    vtxMass = ROOT.vector('float')
+
+    btag = ROOT.vector('std::map<std::string, float>')
--- python/Producers/Jets.py
+++ python/Producers/Jets.py
@@ -5,11 +5,14 @@ from Producer import Producer
 
 from Helper import fill_candidate
 
+from ROOT import std
+
 class Jets(Producer):
 
-    def __init__(self, name, prefix, jet_collection):
+    def __init__(self, name, prefix, jet_collection, btag_collections=[]):
         Producer.__init__(self, name)
 
+        self.btag_collections = btag_collections
         self.uses(name, 'std::vector<pat::Jet>', jet_collection)
         self.produces(Models.Jets.Jets, name, prefix)
 
@@ -19,5 +22,18 @@ class Jets(Producer):
         for jet in jets:
             fill_candidate(jet, product)
 
+            product.jecFactor.push_back(jet.jecFactor(0))
+            product.area.push_back(jet.jetArea())
+            product.partonFlavor.push_back(jet.partonFlavour())
+            product.hadronFlavor.push_back(jet.hadronFlavour())
+
             if jet.hasUserFloat('pileupJetId:fullDiscriminant'):
                 product.pu_jet_id.push_back(jet.userFloat('pileupJetId:fullDiscriminant'))
+
+            product.vtxMass.push_back(jet.userFloat('vtxMass'))
+
+            btaggers = std.map('std::string, float')()
+            for btagger in self.btag_collections:
+                btaggers[btagger] = jet.bDiscriminator(btagger)
+
+            product.btag.push_back(btaggers)
