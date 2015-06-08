ed5b4bc6fe5f136873026d867605c7c516644dcc
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-06-09 00:04:20 +0200

    EA isolation support for muons

--- python/TestConfiguration.py
+++ python/TestConfiguration.py
@@ -18,13 +18,19 @@ class TestConfiguration(Configuration):
     electron_effective_areas = parse_effective_areas_file("RecoEgamma/ElectronIdentification/data/PHYS14/"
                                                           "effAreaElectrons_cone03_pfNeuHadronsAndPhotons.txt")
 
+    muon_effective_areas_R03 = parse_effective_areas_file("cp3_llbb/ExTreeMaker/data/"
+                                                          "effAreaMuons_cone03_pfNeuHadronsAndPhotons.txt")
+    muon_effective_areas_R04 = parse_effective_areas_file("cp3_llbb/ExTreeMaker/data/"
+                                                          "effAreaMuons_cone04_pfNeuHadronsAndPhotons.txt")
+
     producers = [
         Bunch(alias='vertices', clazz=Vertices, vertex_collection='offlineSlimmedPrimaryVertices'),
         Bunch(alias='jets', clazz=Jets, prefix='jet_', jet_collection='slimmedJets',
               btag_collections=['pfCombinedInclusiveSecondaryVertexV2BJetTags']),
         Bunch(alias='puppiJets', clazz=Jets, prefix='puppijet_', jet_collection='slimmedJetsPuppi'),
         Bunch(alias='muons', clazz=Muons, prefix='muon_', muon_collection='slimmedMuons',
-              vertex_collection='offlineSlimmedPrimaryVertices'),
+              vertex_collection='offlineSlimmedPrimaryVertices', effective_areas_R03=muon_effective_areas_R03,
+              effective_areas_R04=muon_effective_areas_R04),
         Bunch(alias='electrons', clazz=Electrons, prefix='electron_', electron_collection='slimmedElectrons',
               effective_areas_R03=electron_effective_areas),
         Bunch(alias='mets', clazz=METs, prefix='met_', met_collection='slimmedMETs')
